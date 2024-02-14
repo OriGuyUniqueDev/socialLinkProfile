cbde42343853b6c3956335d019c3e986d533c8aa
OriGuyUniqueDev <origuydev@gmail.com>
2024-02-14 12:40:15 +0200

    apply theme in tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,17 @@
 export default {
 	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
 	theme: {
+		colors: {
+			black: "#141414",
+			"dark-gray": "#1f1f1f",
+			gray: "#333333",
+			white: "#FFFFFF",
+			"neon-green": "#C4F82A",
+		},
+		fontFamily: {
+			sans: ["Inter", "sans-serif"],
+		},
+		fontSize: {},
 		extend: {},
 	},
 	plugins: [],
